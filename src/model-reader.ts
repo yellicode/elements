ea06721b3f5b68c140fb31da644862d4b8bb723a
Maurice de Laat <info@mauricedelaat.nl>
2018-05-03 13:09:56 +0200

    Refactored ModelReader, making its public interface more flexible.

--- src/model-reader.ts
+++ src/model-reader.ts
@@ -9,57 +9,69 @@ import * as Interfaces from './interfaces';
 import * as Data from './data-interfaces';
 import { DataToModelConverter } from './data-to-model-converter';
 import { ElementTypeUtility } from './utils';
+import { Document } from './document';
 
 export class ModelReader {
-    private converter: DataToModelConverter;
+    private static converter: DataToModelConverter;
 
     /**
      * Contains all profiles that were read from the main document or any referenced documents.
      */
-    private profiles: Interfaces.Profile[] = [];
-
-    constructor() {
-        this.converter = new DataToModelConverter()
-    }
-
+    private static allProfiles: Interfaces.Profile[];
+   
     public static canRead(data: any): boolean {
         if (!data.modelTypeName) return false;
         return ("yellicode yml" === data.modelTypeName.toLowerCase());
-        // throw `Cannot read the model file. Unknown model type '${data.modelTypeName}'.`;        
     }
 
-    public read(documentData: Data.DocumentData): Interfaces.Model | null {
-        if (!documentData.model) {
-            console.error(`The document does not contain any model data.`);
-            return null;
-        }
-
-        const mainModel = this.loadDocumentRecursive(documentData, false);
-        return mainModel;
+    public static readDocument(documentData: Data.DocumentData): Interfaces.Document | null {
+        // Reset
+        ModelReader.allProfiles = [];
+        ModelReader.converter = new DataToModelConverter();
+        return ModelReader.readDocumentRecursive(documentData, false);
     }
 
-    private loadDocumentRecursive(documentData: Data.DocumentData, isReferencedDocument: boolean): Interfaces.Model | null {
+    // public static readModel(modelData: Data.ModelData): Interfaces.Model | null {
+    //     if (!modelData) return null;
+
+    //     const converter = new DataToModelConverter();
+
+    //     return converter.convert(modelData, null, null);
+    // }
+
+    private static readDocumentRecursive(documentData: Data.DocumentData, isReferencedDocument: boolean): Interfaces.Document | null {
+        // Ids must be prefixed if we are loading a referenced document, because references to it 
+        // are prefixed with the document id      
         const elementIdPrefix = isReferencedDocument ? documentData.id : null;
 
-        // Load the document's referenced documents first
-        this.loadReferencedDocumentsRecursive(documentData);
+        // Convert the document's referenced documents first
+        ModelReader.readReferencedDocumentsRecursive(documentData);
 
-        // Read the document's profiles first (this allows the converter to resolve references to them)
-        const profilesModel = documentData.profiles ? this.converter.convert(documentData.profiles, null, elementIdPrefix) : null;
-        if (profilesModel) {
-            profilesModel.packagedElements.forEach(p => {
+        // Convert the document's profiles first (this allows the converter to resolve references to them)
+        const profilesInDocument = documentData.profiles ? ModelReader.converter.convert(documentData.profiles, null, elementIdPrefix) : null;
+        if (profilesInDocument) {
+            profilesInDocument.packagedElements.forEach(p => {
                 if (ElementTypeUtility.isProfile(p.elementType)) { // the element can also be a Enum or other type
-                    this.profiles.push(p as Interfaces.Profile);
+                    ModelReader.allProfiles.push(p as Interfaces.Profile);
                 }
             });
         }
 
-        // Then read the model, using the profiles
-        const model = documentData.model ? this.converter.convert(documentData.model, this.profiles, elementIdPrefix) : null;
-        return model;
+        // Then convert the model, using the profiles
+        const modelInDocument = documentData.model ? ModelReader.converter.convert(documentData.model, ModelReader.allProfiles, elementIdPrefix) : null;
+        const document = new Document(ModelReader.converter.modelDelegate);
+        document.id = documentData.id;
+        document.modelTypeName = documentData.modelTypeName;
+        document.modelTypeVersion = documentData.modelTypeVersion;
+        document.creator =  documentData.creator;
+        document.profiles = profilesInDocument,
+        document.model = modelInDocument;
+        return document;       
+
+        //return documentData.model ? ModelReader.converter.convert(documentData.model, ModelReader.profiles, elementIdPrefix) : null;
     }
 
-    private loadReferencedDocumentsRecursive(documentData: Data.DocumentData): void {
+    private static readReferencedDocumentsRecursive(documentData: Data.DocumentData): void {
         if (!documentData.references)
             return;
 
@@ -68,7 +80,7 @@ export class ModelReader {
             // Load the document. This will load the document's model elements into memory so that the converter can 
             // resolve any references to it. We don't use the referenced document's main model here, it is not returned 
             // through the API but may be referenced by the main document.
-            this.loadDocumentRecursive(r.document, true);
+            ModelReader.readDocumentRecursive(r.document, true);
         });
     }
 }
